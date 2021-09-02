ec03d3fb147fc57bfc94da5e7f108e13b44cba1a
DONG WOO SOHN <samsohn0128@gmail.com>
2021-09-02 02:28:22 -0700

    Matrix Addition

--- src/Driver.java
+++ src/Driver.java
@@ -11,7 +11,8 @@ public class Driver {
 			// pgd.addClass("wordcount", Wordcount.class, "A map/reduce program that performs word counting.");
 			// pgd.addClass("wordcount1char", Wordcount1char.class, "A map/reduce program that counts the 1st character of words in the input files.");
 			// pgd.addClass("wordcountsort", Wordcountsort.class, "A map/reduce program that output frequency of the words in the input files by alphabetical order.");
-			pgd.addClass("inverted", InvertedIndex.class, "A map/reduce program that generates the inverted index using words in the input files.");
+			// pgd.addClass("inverted", InvertedIndex.class, "A map/reduce program that generates the inverted index using words in the input files.");
+			pgd.addClass("matadd", MatrixAdd.class, "A map/reduce program that computes the addition of two matrices.");
 
       			pgd.driver(args);
 			exitCode = 0;
