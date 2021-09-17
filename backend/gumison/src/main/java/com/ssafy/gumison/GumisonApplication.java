6b469a645f5a7d5f9c8653d0b8aabbe46fbe6038
DONGWOO SOHN <samsohn0128@gmail.com>
2021-09-17 16:49:35 +0900

    feat: init project

---
+++ backend/gumison/src/main/java/com/ssafy/gumison/GumisonApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.gumison;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GumisonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GumisonApplication.class, args);
+	}
+
+}
