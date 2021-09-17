5e9adc34dee37973211c1607cc67b228abd4a361
DONGWOO SOHN <samsohn0128@gmail.com>
2021-09-17 18:39:07 +0900

    chore: nothing changed

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>hi</div>
+  <Footer/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Footer from '@/components/Footer.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Footer,
   }
 }
 </script>
---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="footer-bar" class="footer-bar-1 position-relative mb-4 ms-3 me-3 shadow-xl rounded-s">
+        <a href="#"><i class="fa fa-home"></i><span>Home</span></a>
+        <a href="#"><i class="fa fa-star"></i><span>Features</span></a>
+        <a href="#" class="active-nav"><i class="fa fa-heart"></i><span>Pages</span></a>
+        <a href="#"><i class="fa fa-search"></i><span>Search</span></a>
+        <a href="#"><i class="fa fa-cog"></i><span>Settings</span><em class="badge bg-highlight">3</em></a>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Footer',
+}
+</script>
+
+<style>
+
+</style>
