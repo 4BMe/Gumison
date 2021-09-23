1b5a4a74e77f9217aaf9d11da9ebb1f50a0debef
Minji Kim <mjkim103301@gmail.com>
2021-09-23 16:53:13 +0900

    fix: jpa 관련 수정
    오타 수정 및 그래들 코드 위치 변경

--- backend/gumison/src/main/java/com/ssafy/gumison/api/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.gumison.api.service;
-
-/**
- *	유저 관련 비즈니스 로직 처리를 위한 서비스 인터페이스 정의.
- */
-public interface UserService {
-}
--- backend/gumison/src/main/java/com/ssafy/gumison/db/repository/SolutionRepository.java
+++ backend/gumison/src/main/java/com/ssafy/gumison/db/repository/SolutionRepository.java
@@ -3,6 +3,6 @@ package com.ssafy.gumison.db.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.ssafy.gumison.db.entity.SolutionEntity;
 
-public interface SolutionRepository extends JpaRepository<SolutionEntity, Long> { {
+public interface SolutionRepository extends JpaRepository<SolutionEntity, Long> {
 
 }
