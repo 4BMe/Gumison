827894b8878fb5e0a2d8105741eb28b26ac60562
yhpgKIM <greenboy94@naver.com>
2021-09-23 16:54:10 +0900

    feat: init branch

--- backend/gumison/src/main/java/com/ssafy/gumison/db/repository/LevelTierRepository.java
+++ backend/gumison/src/main/java/com/ssafy/gumison/db/repository/LevelTierRepository.java
@@ -1,8 +1,5 @@
 package com.ssafy.gumison.db.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.ssafy.gumison.db.entity.LevelTierEntity;
-
-public interface LevelTierRepository extends JpaRepository<LevelTierEntity, Long> {
+public class LevelTierRepository {
 
 }
