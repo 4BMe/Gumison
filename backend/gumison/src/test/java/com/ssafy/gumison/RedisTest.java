6c3403639d53ac92d8284991ce0abb38934722fb
cherrytomato1 <terec@naver.com>
2021-09-28 13:07:05 +0900

    test: ZSet 테스트
    ZSet(sortSet) CRU 테스트

--- backend/gumison/src/test/java/com/ssafy/gumison/RedisTest.java
+++ backend/gumison/src/test/java/com/ssafy/gumison/RedisTest.java
@@ -5,14 +5,19 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.data.redis.core.ZSetOperations;
 
 @SpringBootTest
 public class RedisTest {
 
 	@Autowired
 	private StringRedisTemplate stringRedisTemplate;
+
+	@Autowired
+	private RedisTemplate<String, Object> redisTemplate;
 
 	@Test
 	public void 값_설정_및_조회_삭제_기능을_수행한다() {
@@ -28,4 +33,24 @@ public class RedisTest {
 		Assertions.assertNull(valueOperations.get(KEY));
 	}
 
+	@Test
+	public void ZSET에_값_설정_조회_삭제_기능을_수행한다() {
+
+		final String KEY = "TEST KEY";
+		final Integer[] SCORES = {300, 500, 100};
+		final String[] VALUES = {"testVal 0", "testVal 1", "testVal 2"};
+
+		ZSetOperations<String, Object> zSetOperations = redisTemplate.opsForZSet();
+
+		for (int i = 0; i < SCORES.length; i++) {
+			Assertions.assertTrue(zSetOperations.add(KEY, VALUES[i], SCORES[i]).booleanValue());
+		}
+
+		Assertions.assertEquals(zSetOperations.rank(KEY, VALUES[2]), 0);
+
+		for (int i = 0; i < SCORES.length ; i++) {
+			Assertions.assertNotNull(zSetOperations.remove(KEY, VALUES[i]));
+		}
+
+	}
 }
