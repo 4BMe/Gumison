906f0f92cbc1234340ba1adbf63d5a24296ea52e
yhpgKIM <greenboy94@naver.com>
2021-09-28 11:20:08 +0900

    feat: security config

--- backend/gumison/src/main/java/com/ssafy/gumison/config/SecurityConfig.java
+++ backend/gumison/src/main/java/com/ssafy/gumison/config/SecurityConfig.java
@@ -1,19 +1,20 @@
 package com.ssafy.gumison.config;
 
-import com.ssafy.gumison.security.oauth2.CustomOAuth2UserService;
-import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import com.ssafy.gumison.security.oauth2.CustomOAuth2UserService;
+import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @EnableWebSecurity // Spring Security 설정 활성화
-public class SecurityConfig {
-  // extends WebSecurityConfigurerAdapter {}
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   private final CustomOAuth2UserService customOAuth2UserService;
 
-  //@Override
+  @Override
   protected void configure(HttpSecurity http) throws Exception {
     http
         .csrf().disable()
@@ -34,6 +35,5 @@ public class SecurityConfig {
         // 소셜로그인 성공 시 후속 조치를 진행할 UserService 인터페이스 구현체 등록
         .userService(customOAuth2UserService); // 리소스 서버에서 사용자 정보를 가져온 상태에서 추가로 진행하고자 하는 기능 명시
 
-    // super.configure(http);
   }
 }
