9e166031b97d73caffa756b495caff36ccfeeb1d
손동우 <samsohn0128@gmail.com>
2021-09-29 02:21:49 +0000

    feat: CREATE & UPDATE Solution

--- backend/gumison/src/main/java/com/ssafy/gumison/db/entity/LevelTier.java
+++ backend/gumison/src/main/java/com/ssafy/gumison/db/entity/LevelTier.java
@@ -12,6 +12,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Builder
@@ -19,6 +20,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @Getter
 @Setter
+@ToString
 public class LevelTier {
 
   @Id
