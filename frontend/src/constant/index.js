2e5714c5f5d3407bd261e6ef3405fc0d65331e6a
Minji Kim <mjkim103301@gmail.com>
2021-10-01 10:31:41 +0900

    fix: frontend 포트 변경, favicon.ico 추가

--- frontend/src/constant/index.js
+++ frontend/src/constant/index.js
@@ -1,5 +1,5 @@
 const BASE_URL = "http://localhost:8888/api";
-const GOOGLE_OAUTH_URL = "http://localhost:8888/oauth2/authorize/google?redirect_uri=http://localhost:8081/oauth2/redirect";
+const GOOGLE_OAUTH_URL = "http://localhost:8888/oauth2/authorize/google?redirect_uri=http://localhost:9000/oauth2/redirect";
 
 module.exports = {
     BASE_URL,
