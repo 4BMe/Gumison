a3d84e3635d9e7a6a3637b302aee10fc2ea7b184
DONGWOO SOHN <samsohn0128@gmail.com>
2021-10-01 22:16:20 +0900

    feat: Create Solution with Videos

--- backend/gumison/src/main/java/com/ssafy/gumison/api/controller/HistoryController.java
+++ backend/gumison/src/main/java/com/ssafy/gumison/api/controller/HistoryController.java
@@ -1,8 +1,10 @@
 package com.ssafy.gumison.api.controller;
 
 import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -11,9 +13,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.gumison.api.request.SolutionRequest;
+import com.ssafy.gumison.api.response.HistoryRes;
 import com.ssafy.gumison.api.response.SolutionListRes;
 import com.ssafy.gumison.api.response.SolutionRes;
-import com.ssafy.gumison.api.response.UserHistoryRes;
 import com.ssafy.gumison.api.service.HistoryService;
 import com.ssafy.gumison.common.response.ApiResponseDto;
 import com.ssafy.gumison.db.entity.Solution;
@@ -31,8 +33,9 @@ public class HistoryController {
 
   @ApiOperation(value = "유저 히스토리", notes = "히스토리를 보여줍니다.", response = ApiResponseDto.class)
   @GetMapping("/{nickname}")
-  public ApiResponseDto<UserHistoryRes> userHistory(@PathVariable("nickname") String nickname) {
-    UserHistoryRes historyRes = historyService.history(nickname);
+  public ApiResponseDto<HistoryRes> userHistory(@PathVariable("nickname") String nickname) {
+    HistoryRes historyRes = historyService.history(nickname);
+    log.info(historyRes.getUser().getNickname());
     return ApiResponseDto.success(historyRes);
   }
 
@@ -51,21 +54,18 @@ public class HistoryController {
 
   @ApiOperation(value = "레벨 기록 생성", notes = "성공한 레벨 기록을 생성합니다.", response = ApiResponseDto.class)
   @PostMapping("/")
-  public ApiResponseDto<Solution[]> createSolution(@RequestBody SolutionRequest[] solutionRequests) {
-    log.info("[createSolution] : " + Arrays.toString(solutionRequests));
-    Solution[] solutions = new Solution[solutionRequests.length];
-    for (int i = 0; i < solutionRequests.length; i++) {
-      Solution solution = historyService.createSolution(solutionRequests[i]);
-      solutions[i] = solution;
-    }
+  public ApiResponseDto<List<Solution>> createSolution(@ModelAttribute SolutionRequest solutionRequest) {
+    log.info("[createSolution] - HistoryController : {}", solutionRequest);
+    List<Solution> solutions = historyService.createSolution(solutionRequest);
     return ApiResponseDto.success(solutions);
   }
 
   @ApiOperation(value = "레벨 기록 수정", notes = "성공한 레벨 기록을 수정합니다.", response = ApiResponseDto.class)
-  @PutMapping("/{solutionId}")
-  public ApiResponseDto<Solution> updateSolution(@PathVariable String solutionId,
-      @RequestBody SolutionRequest solutionRequest) {
-    Solution solution = historyService.updateSolution(solutionId, solutionRequest);
-    return ApiResponseDto.success(solution);
+  @PutMapping("/")
+  public ApiResponseDto<Solution[]> updateSolution(@RequestBody SolutionRequest[] solutionRequests) {
+    log.info("[updateSolution] - HistoryController : {}", Arrays.toString(solutionRequests));
+    Solution[] solutions = new Solution[solutionRequests.length];
+
+    return ApiResponseDto.success(solutions);
   }
 }
--- backend/gumison/src/main/java/com/ssafy/gumison/api/service/VideoService.java
+++
@@ -1,8 +0,0 @@
-package com.ssafy.gumison.api.service;
-
-import com.ssafy.gumison.api.request.VideoRequest;
-import com.ssafy.gumison.db.entity.SolutionVideo;
-
-public interface VideoService {
-  public SolutionVideo[] uploadVideo(VideoRequest videoRequest);
-}
