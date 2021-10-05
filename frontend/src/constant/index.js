9782d765e75234aa6cfd77bc0006e36de2e6155d
yhpgKIM <greenboy94@naver.com>
2021-10-05 22:23:21 +0900

    fix: typo front base_url

--- frontend/src/constant/index.js
+++ frontend/src/constant/index.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://i5a104.p.ssafy.io:8888/api";
+const BASE_URL = "https://j5a202.p.ssafy.io:8888/api";
 const GOOGLE_OAUTH_URL = "http://localhost:8888/oauth2/authorize/google?redirect_uri=http://localhost:9000/oauth2/redirect";
 
 module.exports = {
