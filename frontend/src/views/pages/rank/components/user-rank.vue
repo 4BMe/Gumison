0e110aadf10dd6529037105c966865cb1e99b007
cherrytomato1 <terec@naver.com>
2021-10-06 13:12:10 +0900

    feat: 랭크 페이지 사용자 프로필 라우터 추가

--- frontend/src/views/pages/rank/components/user-rank.vue
+++ frontend/src/views/pages/rank/components/user-rank.vue
@@ -1,54 +1,56 @@
 <template>
   <div class="bg-light" v-if="user">
-    <router-link to="/myhistory">
-      <div class="media">
-        <div class="avatar-xs overflow-hidden mr-3 align-self-center">
-          <span
-            class=" avatar-title text-primary rounded-circle bg-soft-light align-center"
-            >{{ user.rank }}
-          </span>
-        </div>
-        <div class="chat-user-img online align-self-center mr-3">
-          <div v-if="user.profile">
-            <img :src="user.profile" class="rounded-circle avatar-xs" alt />
-          </div>
-          <div class="avatar-xs" v-if="!user.profile">
+    <div @click="searchHistory(user.nickname)">
+      <a href="javascript:void(0);">
+        <div class="media">
+          <div class="avatar-xs overflow-hidden mr-3 align-self-center">
             <span
-              class="avatar-title rounded-circle bg-soft-primary text-primary"
-              >{{ $t(user.nickname).charAt(0) }}
+              class=" avatar-title text-primary rounded-circle bg-soft-light align-center"
+              >{{ user.rank }}
             </span>
           </div>
-        </div>
+          <div class="chat-user-img online align-self-center mr-3">
+            <div v-if="user.profile">
+              <img :src="user.profile" class="rounded-circle avatar-xs" alt />
+            </div>
+            <div class="avatar-xs" v-if="!user.profile">
+              <span
+                class="avatar-title rounded-circle bg-soft-primary text-primary"
+                >{{ $t(user.nickname).charAt(0) }}
+              </span>
+            </div>
+          </div>
 
-        <div class="media-body overflow-hidden">
-          <h5 class="text-truncate font-size-15 mb-1">
-            <img
-              :src="require(`@/assets/images/tier/` + user.tier + `.png`)"
-              alt="user-img"
-              class="img-fluid rounded-circle sm-tier-img"
-            />
-            {{ user.nickname }}
-          </h5>
-          <table style="width:100%">
-            <tbody>
-              <tr>
-                <td style="width:0px">
-                  <p class="chat-user-message text-truncate mb-0">
-                    {{ user.tier }}
-                  </p>
-                </td>
-                <td>
-                  <p class="chat-user-message text-truncate mb-0">
-                    <b>{{ user.solCnt }}</b> 문제 성공
-                  </p>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+          <div class="media-body overflow-hidden">
+            <h5 class="text-truncate font-size-15 mb-1">
+              <img
+                :src="require(`@/assets/images/tier/` + user.tier + `.png`)"
+                alt="user-img"
+                class="img-fluid rounded-circle sm-tier-img"
+              />
+              {{ user.nickname }}
+            </h5>
+            <table style="width:100%">
+              <tbody>
+                <tr>
+                  <td style="width:0px">
+                    <p class="chat-user-message text-truncate mb-0">
+                      {{ user.tier }}
+                    </p>
+                  </td>
+                  <td>
+                    <p class="chat-user-message text-truncate mb-0">
+                      <b>{{ user.solCnt }}</b> 문제 성공
+                    </p>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
-      </div>
-      <!-- End chat-message-list -->
-    </router-link>
+      </a>
+    </div>
+    <!-- End chat-message-list -->
   </div>
 </template>
 
@@ -79,6 +81,9 @@ export default {
       let ret = await getUserRankByNickname(nickname);
       console.log(ret);
       this.user = ret.userRank;
+    },
+    searchHistory(nickname) {
+      this.$router.push({ name: "myhistory", params: { nickname: nickname } });
     },
   },
 };
